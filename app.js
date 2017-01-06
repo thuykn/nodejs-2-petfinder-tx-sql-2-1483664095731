91c074f23e508078108ed8d48e014f2e3d9bc07a
thuykn <thuykn@us.ibm.com>
2017-01-05 20:25:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,17 +12,59 @@ var express = require('express');
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+// New code for route
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+var ibmdb = require('ibm_db');
+require('cf-deployment-tracker-client').track();
+
 // create a new express server
 var app = express();
-
+//New code for all env
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+var db2;
+var hasConnect = false;
 
+// New code
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+if (process.env.VCAP_SERVICES) {
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+	if (env['dashdb_tx_sql_2']) {
+        hasConnect = true;
+		db2 = env['dashdb_tx_sql_2'][0].credentials;
+	}
+	
+}
+var connString = "DRIVER={DB2};DATABASE=" + db2.db + ";UID=" + db2.username + ";PWD=" + db2.password + ";HOSTNAME=" + db2.hostname + ";port=" + db2.port;
+
+app.get('/', routes.listSysTables(ibmdb,connString));
+
+http.createServer(app).listen(app.get('port'), function(){
+console.log('Express server listening on port ' + app.get('port'));
+});
 // get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+//var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+//app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+ // console.log("server starting on " + appEnv.url);
+//});
